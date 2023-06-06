b0b38f86f0afdab572d4ba2a82a8736d0f9b0dd0
Alexis Sayago <as.sayagom@gmail.com>
2023-06-05 21:06:03 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='display-3 fw-bold text-secondary'>Traffic Light</h1>
+      <div className='trafficLightWhole'>
+        <div className="trafficLight">
+          <div className="red"></div>
+          <div className="yellow"></div>
+          <div className="green"></div>
+        </div>
+        <div className='trafficLightBottom'></div>
+      </div>
     </div>
   );
 }
