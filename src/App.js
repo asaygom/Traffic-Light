25db8e9d1b5c8c8771325bb1bc57e6fec2f794fc
Alexis Sayago <as.sayagom@gmail.com>
2023-06-07 20:15:43 -0400

    project working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [activeLight, setActiveLight] = useState("")
+
   return (
     <div className="App">
       <h1 className='display-3 fw-bold text-secondary'>Traffic Light</h1>
       <div className='trafficLightWhole'>
         <div className="trafficLight">
-          <div className="red"></div>
-          <div className="yellow"></div>
-          <div className="green"></div>
+          <div className={"red"+(activeLight==="red"?" selected":"")} onClick={() => {setActiveLight("red")}}></div>
+          <div className={"yellow"+(activeLight==="yellow"?" selected":"")} onClick={() => {setActiveLight("yellow")}}></div>
+          <div className={"green"+(activeLight==="green"?" selected":"")} onClick={() => {setActiveLight("green")}}></div>
         </div>
         <div className='trafficLightBottom'></div>
       </div>
